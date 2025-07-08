8a5c7bd6e9865efba80391073e03592d352210d8
Aki Sasaki <aki@escapewindow.com>
2025-07-08 17:47:04 -0600

    repeat key on layer 1?

--- AE5bZ/keymap.c
+++ AE5bZ/keymap.c
@@ -30,7 +30,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
   [1] = LAYOUT_voyager(
     KC_TRANSPARENT, KC_1,           KC_2,           KC_3,           KC_4,           KC_5,                                           KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_TRANSPARENT, 
-    KC_TRANSPARENT, MT(MOD_LALT, KC_BSLS),MT(MOD_LGUI, KC_MINUS),MT(MOD_LCTL, KC_LBRC),MT(MOD_LSFT, KC_RBRC),KC_NO,                                          KC_LEFT,        MT(MOD_RSFT, KC_DOWN),MT(MOD_RCTL, KC_UP),MT(MOD_RGUI, KC_RIGHT),MT(MOD_RALT, KC_QUOTE),KC_TRANSPARENT, 
+    KC_TRANSPARENT, MT(MOD_LALT, KC_BSLS),MT(MOD_LGUI, KC_MINUS),MT(MOD_LCTL, KC_LBRC),MT(MOD_LSFT, KC_RBRC),QK_REP,                                          KC_LEFT,        MT(MOD_RSFT, KC_DOWN),MT(MOD_RCTL, KC_UP),MT(MOD_RGUI, KC_RIGHT),MT(MOD_RALT, KC_QUOTE),KC_TRANSPARENT, 
     KC_TRANSPARENT, KC_LEFT_SHIFT,  CW_TOGG,        TD(DANCE_0),    TD(DANCE_1),    QK_LLCK,                                        KC_NO,          KC_GRAVE,       KC_EQUAL,       KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, 
     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TAB,         TO(0),                                          KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, 
                                                     KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT
